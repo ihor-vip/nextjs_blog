d37f1dc4870c1f4e00385c6312ec16ed1496c3b4
ihor-vip <vip20140902@gmail.com>
2023-05-21 11:46:12 +0300

    install dependency, add styling and icons

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,20 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import "@styles/globals.css";
 export const metadata = {
   title: 'Next Js AI',
   description: 'Next Js app using AI',
 }
+const RootLayout = ({ children }) => (
+    <html lang='en'>
+    <body>
+      <div className='main'>
+        <div className='gradient' />
+      </div>
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <main className='app'>
+        {children}
+      </main>
+    </body>
     </html>
-  )
-}
+);
+
+export default RootLayout;
